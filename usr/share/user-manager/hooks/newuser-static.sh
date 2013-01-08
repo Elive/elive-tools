c1c5a095d52d680bf52e873639927469b3ce6840
Thanatermesis <thanatermesis@gmail.com>
2013-01-08 18:01:07 +0100

    fix

--- usr/share/user-manager/hooks/newuser-static.sh
+++ usr/share/user-manager/hooks/newuser-static.sh
@@ -5,6 +5,9 @@ main(){
     # pre {{{
     local NUMBERRANDOM username
 
+    # input
+    username="$1"
+
     # Usage
     if [[ -z "${1}" ]] ; then
         echo -e "Usage: $(basename $BASH_SOURCE) username"
@@ -20,9 +23,6 @@ main(){
     if [[ -f "/etc/adduser.conf" ]] ; then
         source /etc/adduser.conf
     fi
-
-    # input
-    username="$1"
 
 
     # }}}

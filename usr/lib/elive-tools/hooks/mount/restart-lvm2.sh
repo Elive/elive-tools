52a03c212f3959f1026b4380a98795108b419ee8
Thanatermesis <thanatermesis@gmail.com>
2013-05-27 02:30:37 +0200

    restart lvm2 hook script added

---
+++ usr/lib/elive-tools/hooks/mount/restart-lvm2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# NOTE: actually our thunar hacks doesn't seems to see when a device is unencrypted, only mounted
+
+# restart lvm2 service
+#######################
+#   useful to show lvm2 devices after to mount a crypted fs, at least on live mode (sudo enabled)
+if [[ -x "/etc/init.d/lvm2" ]] ; then
+    if [[ "$UID" = 0 ]] ; then
+	invoke-rc.d lvm2 start
+    else
+	# running as user (maybe in live)
+	source /usr/lib/elive-tools/functions
+	if el_check_sudo_automated ; then
+	    sudo invoke-rc.d lvm2 start
+	fi
+    fi
+fi
+

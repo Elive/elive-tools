dbce6a4ca390f35442f684dab02f80f9ead30aee
Thanatermesis <thanatermesis@gmail.com>
2013-08-17 02:03:37 +0200

    warn users about that NTFS can kill his cat

---
+++ usr/lib/elive-tools/hooks/mount/post.mount.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#source /usr/lib/elive-tools/functions
+set -x
+
+main(){
+    # pre {{{
+    local filesystem mountpoint partition label
+
+    label="$1"
+    partition="$2"
+    mountpoint="$3"
+    filesystem="$4"
+
+    # }}}
+
+
+    if [[ "$filesystem" = ntfs* ]] || [[ "${filesystem}" = NTFS* ]] ; then
+	is_ntfs=1
+    else
+	if [[ -z "${filesystem}" ]] || [[ "$filesystem" = fuse* ]] ; then
+	    if file -s "${partition}" | grep -isq "ntfs" ; then
+		is_ntfs=1
+	    fi
+
+	fi
+    fi
+
+
+    if ((is_ntfs)) ; then
+	warning-messages-elive ntfs_is_crap
+    fi
+
+
+}
+
+#
+#  MAIN
+#
+main "$@"
+
+# vim: set foldmethod=marker :
--- usr/lib/elive-tools/hooks/mount/pre.umount.sh
+++ usr/lib/elive-tools/hooks/mount/pre.umount.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 #source /usr/lib/elive-tools/functions
 
-# kill sucky process that doesn't allow us to umount devices
-if top -b -n 1 | head -n 14 | tail -n 7  | grep -q tumblerd 2>/dev/null 1>/dev/null ; then
-    killall tumblerd 2>/dev/null 1>/dev/null
-    killall -9 tumblerd 2>/dev/null 1>/dev/null
-fi
+main(){
+    # pre {{{
+    local filesystem mountpoint partition label
 
+    label="$1"
+    partition="$2"
+    mountpoint="$3"
+    filesystem="$4"
+
+    # }}}
+
+
+    # kill sucky process that doesn't allow us to umount devices
+    if top -b -n 1 | head -n 14 | tail -n 7  | grep -q tumblerd 2>/dev/null 1>/dev/null ; then
+	killall tumblerd 2>/dev/null 1>/dev/null
+	killall -9 tumblerd 2>/dev/null 1>/dev/null
+    fi
+
+}
+
+#
+#  MAIN
+#
+main "$@"
+
+# vim: set foldmethod=marker :

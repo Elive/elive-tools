762d71b860484bd72e3058dd808bf2f73d9e8f4e
Thanatermesis <thanatermesis@gmail.com>
2014-04-04 13:14:58 +0200

    improvings

--- etc/zsh/zprofile.d/inform-remote-connection.sh
+++ etc/zsh/zprofile.d/inform-remote-connection.sh
@@ -1,3 +1,15 @@
+LOCKFILE="/tmp/.inform-ssh-remote-${USER}-$(basename $0).lock"
+[[ -r $LOCKFILE ]] && PROCESS=$(cat $LOCKFILE) || PROCESS=" "
+if (ps -p $PROCESS) >/dev/null 2>&1
+then
+    # already running
+    return
+else
+    rm -f $LOCKFILE
+    echo $$ > $LOCKFILE
+fi
+
+# which remote ip?
 if [[ -n "$SSH_CLIENT" ]] ; then
     SSH_REMOTE_IP="${SSH_CLIENT%% *}"
 fi
@@ -8,7 +20,6 @@ fi
 if [[ -z "$SSH_REMOTE_IP" ]] && [[ -n "$SSH2_IP" ]] ; then
     SSH_REMOTE_IP="${SSH2_IP%% *}"
 fi
-
 
 
 # For test, you can try to set this ip which should reference to India
@@ -30,13 +41,15 @@ if [[ -n "$SSH_REMOTE_IP" ]] ; then
         SSH_REMOTE_DATA="$( showmylocation "$SSH_REMOTE_IP" )"
         echo "$SSH_REMOTE_DATA" > "/tmp/.ssh_remote_data-${USER}:${SSH_REMOTE_IP}"
     fi
-fi
 
-if [[ -n "$SSH_REMOTE_DATA" ]] ; then
+    # get the rest of data
     SSH_REMOTE_COUNTRY="$( echo "$SSH_REMOTE_DATA" | grep CountryName | sed -e 's|</Country.*$||g' -e 's|^.*Name>||g' )"
     SSH_REMOTE_REGION="$( echo "$SSH_REMOTE_DATA" | grep RegionName | sed -e 's|</Region.*$||g' -e 's|^.*Name>||g' )"
     SSH_REMOTE_CITY="$( echo "$SSH_REMOTE_DATA" | grep City | sed -e 's|</City.*$||g' -e 's|^.*City>||g' )"
 
+fi
+
+if [[ -n "$SSH_REMOTE_DATA" ]] ; then
     # get local data
     #echo -e "collecting local data..." 1>&2
     if [[ -s "/tmp/.ssh_local_data-${USER}" ]] ; then
@@ -46,19 +59,25 @@ if [[ -n "$SSH_REMOTE_DATA" ]] ; then
         echo "$SSH_LOCAL_DATA" > "/tmp/.ssh_local_data-${USER}"
     fi
 
+    # get the rest of data
     SSH_LOCAL_COUNTRY="$( echo "$SSH_LOCAL_DATA" | grep CountryName | sed -e 's|</Country.*$||g' -e 's|^.*Name>||g' )"
     SSH_LOCAL_REGION="$( echo "$SSH_LOCAL_DATA" | grep RegionName | sed -e 's|</Region.*$||g' -e 's|^.*Name>||g' )"
     SSH_LOCAL_CITY="$( echo "$SSH_LOCAL_DATA" | grep City | sed -e 's|</City.*$||g' -e 's|^.*City>||g' )"
 
+
     # Inform to user about remote connection from...
     if [[ "${SSH_REMOTE_COUNTRY}" != "$SSH_LOCAL_COUNTRY" ]] ; then
         el_speak_text "somebody connected from $SSH_REMOTE_COUNTRY"
+        # wait a small delay for settle the lockfile in slow computers
+        LC_NUMERIC=C sleep 0.8
     else
         if [[ "${SSH_REMOTE_REGION}" != "$SSH_LOCAL_REGION" ]] ; then
             el_speak_text "somebody connected from $SSH_REMOTE_REGION"
+            LC_NUMERIC=C sleep 0.8
         else
             if [[ "${SSH_REMOTE_CITY}" != "$SSH_LOCAL_CITY" ]] ; then
                 el_speak_text "somebody connected from $SSH_REMOTE_CITY"
+                LC_NUMERIC=C sleep 0.8
             fi
         fi
     fi

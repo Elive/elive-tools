0f3b8294ee63c214f57b0e42dc106e7ae2944f08
Thanatermesis <thanatermesis@gmail.com>
2014-05-22 12:46:45 +0200

    multiple fixes and modifications for new user configs

--- etc/user-manager/hooks/e17-wizard/first/first-configurations.sh
+++ etc/user-manager/hooks/e17-wizard/first/first-configurations.sh
@@ -3,17 +3,23 @@ source /usr/lib/elive-tools/functions
 
 main(){
     # pre {{{
-    local NUMBERRANDOM
-
-    # checks
-    if [[ "$USER" = root ]] ; then
-        exit 1
-    fi
-
+    local var
 
     # }}}
 
+    # Audio configurations {{{
+    el_explain 0 "Configuring audio cards..."
+    audio-configurator --quiet
+
+    el_explain 0 "Setting default volumes..."
+    setvolume defaults
+
+    # - Audio configurations }}}
     # add elive gpg key {{{
+
+    # import gnupg keys
+    el_explain 0 "Importing Elive gpg key..."
+
     if [[ -d "/usr/share/elive-security" ]] ; then
         if el_dependencies_check gpg ; then
             gpg --import /usr/share/elive-security/*.asc
@@ -21,8 +27,6 @@ main(){
     fi
 
     # }}}
-
-
 
 }
 
---
+++ etc/user-manager/hooks/e17-wizard/last/xchat-random-names.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+source /usr/lib/elive-tools/functions
+
+
+main(){
+    # pre {{{
+    local ip number md5ip NUMBERRANDOM
+
+    # }}}
+
+    # set xchat random names {{{
+    # checks {{{
+    if el_check_variables "HOME" || ! [[ -d "$HOME" ]]  ; then
+        el_error "no HOME exist for this user? exiting..."
+        sleep 2
+        exit 1
+    fi
+
+    if el_check_variables "LANG" ; then
+        if ! echo "$LANG" | grep -qsi "UTF" ; then
+            el_warning "Your language is not set to UTF-8 mode? '$LANG'"
+        fi
+    else
+        el_warning "LANG variable is not set?"
+    fi
+
+    # - checks }}}
+
+    if [[ -f "$HOME/.xchat2/xchat.conf" ]] ; then
+        ip="$( showmyip )"
+
+        if [[ -n "$ip" ]] ; then
+            md5ip="$( echo "$ip" | md5sum | awk '{print $1}' )"
+
+            NUMBERRANDOM="${md5ip:0:2}"
+            sed -i "s|irc_nick1 = Elive_user|irc_nick1 = Elive_user_${LANG%%_*}_${NUMBERRANDOM}|" "${HOME}/.xchat2/xchat.conf"
+
+            NUMBERRANDOM="${md5ip:2:2}"
+            sed -i "s|irc_nick2 = Elive_user|irc_nick2 = Elive_user_${LANG%%_*}_${NUMBERRANDOM}|" "${HOME}/.xchat2/xchat.conf"
+
+            NUMBERRANDOM="${md5ip:4:2}"
+            sed -i "s|irc_nick3 = Elive_user|irc_nick3 = Elive_user_${LANG%%_*}_${NUMBERRANDOM}|" "${HOME}/.xchat2/xchat.conf"
+        else
+
+            randomized="$RANDOM$RANDOM$RANDOM"
+
+            NUMBERRANDOM="${randomized:0:2}"
+            sed -i "s|irc_nick1 = Elive_user|irc_nick1 = Elive_user_${LANG%%_*}_${NUMBERRANDOM}|" "${HOME}/.xchat2/xchat.conf"
+
+            NUMBERRANDOM="${randomized:2:2}"
+            sed -i "s|irc_nick2 = Elive_user|irc_nick2 = Elive_user_${LANG%%_*}_${NUMBERRANDOM}|" "${HOME}/.xchat2/xchat.conf"
+
+            NUMBERRANDOM="${randomized:4:2}"
+            sed -i "s|irc_nick3 = Elive_user|irc_nick3 = Elive_user_${LANG%%_*}_${NUMBERRANDOM}|" "${HOME}/.xchat2/xchat.conf"
+        fi
+    else
+        el_error "No xchat conf dir exist? ignoring..."
+    fi
+
+
+    # }}}
+
+}
+
+#
+#  MAIN
+#
+main "$@"
+
+# vim: set foldmethod=marker :

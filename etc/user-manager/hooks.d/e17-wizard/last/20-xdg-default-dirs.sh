83297fe10edfe27be6e96b8ca0a4694a9e0d63fd
Thanatermesis <thanatermesis@gmail.com>
2014-06-06 20:52:51 +0200

    much improved migration of user directories

--- etc/user-manager/hooks.d/e17-wizard/last/20-xdg-default-dirs.sh
+++ etc/user-manager/hooks.d/e17-wizard/last/20-xdg-default-dirs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source /usr/lib/elive-tools/functions
 
+# mv dir/* will include hidden files:
+shopt -s dotglob
+
 main(){
     # pre {{{
     local var
@@ -17,28 +20,104 @@ main(){
     xdg-user-dirs-update
     xdg-user-dirs-gtk-update
 
-    # source after to have created it
+    # source after to have created it and dirs
     source "${XDG_CONFIG_HOME}/user-dirs.dirs"
+    cd
 
-    # delete Desktop entry, what a useless idea
-    if [[ -d "$HOME/Desktop" ]] ; then
+    #
+    # Desktop & Downloads
+    #
+
+    # delete Desktop entirely, what a useless idea
+    if [[ -e "$HOME/Desktop" ]] ; then
+
+        mv "$HOME/"Desktop/* "$( xdg-user-dir DOWNLOAD)/" 2>/dev/null || true
         rmdir "$HOME/Desktop" 2>/dev/null 1>&2 || true
 
-        if [[ -d "$HOME/Desktop" ]] ; then
+        # if still exist, move it somewhere that doesn't annoy us
+        if [[ -e "$HOME/Desktop" ]] ; then
             mv "$HOME/Desktop" "$(xdg-user-dir DOWNLOAD)/"
         fi
+
+        # if this is just a symlink (old deprecated dir), safe to remove like this
+        rm -f "$HOME/Downloads" 2>/dev/null 1>&2 || true
     fi
 
+    # remove new xdg desktop dir too
     rmdir "$(xdg-user-dir DESKTOP )" 2>/dev/null 1>&2 || true
 
+    # and never create it again
     sed -i "/^XDG_DESKTOP_DIR/d" "${XDG_CONFIG_HOME}/user-dirs.dirs"
-    echo -e "XDG_DESKTOP_DIR=\"\$HOME/\"" >> "${XDG_CONFIG_HOME}/user-dirs.dirs"
+    #echo -e "XDG_DESKTOP_DIR=\"\$HOME/\"" >> "${XDG_CONFIG_HOME}/user-dirs.dirs"
+    # put it on the same place as downloads, not main homedir
+    grep "^XDG_DOWNLOAD_DIR=" "${XDG_CONFIG_HOME}/user-dirs.dirs" | sed -e 's|_DOWNLOAD_|_DESKTOP_|g' >> "${XDG_CONFIG_HOME}/user-dirs.dirs"
 
 
-    # delete Templates too
+    #
+    # Templates
+    #
+
+    # delete Templates too, they are useless
     rmdir "$(xdg-user-dir TEMPLATES )" 2>/dev/null 1>&2 || true
     sed -i "/^XDG_TEMPLATES_DIR/d" "${XDG_CONFIG_HOME}/user-dirs.dirs"
-    echo -e "XDG_TEMPLATES_DIR=\"\$HOME/\"" >> "${XDG_CONFIG_HOME}/user-dirs.dirs"
+    #echo -e "XDG_TEMPLATES_DIR=\"\$HOME/\"" >> "${XDG_CONFIG_HOME}/user-dirs.dirs"
+    # put it on the same place as documents, not main homedir
+    grep "^XDG_DOCUMENTS_DIR=" "${XDG_CONFIG_HOME}/user-dirs.dirs" | sed -e 's|_DOCUMENTS_|_TEMPLATES_|g' >> "${XDG_CONFIG_HOME}/user-dirs.dirs"
+
+
+    #
+    # Documents
+    #
+
+    if [[ -e "$HOME/Documents" ]] ; then
+        mv "$HOME/"Documents/* "$( xdg-user-dir DOCUMENTS )/" 2>/dev/null || true
+
+        rmdir "$HOME/Documents" 2>/dev/null 1>&2 || true
+
+        # if still exist, move it somewhere that doesn't annoy us
+        mv "$HOME/Documents" "$(xdg-user-dir DOCUMENTS )/" 2>/dev/null || true
+    fi
+
+    #
+    # Music
+    #
+
+    if [[ -e "$HOME/Music" ]] ; then
+        mv "$HOME/"Music/* "$( xdg-user-dir MUSIC )/" 2>/dev/null || true
+
+        rmdir "$HOME/Music" 2>/dev/null 1>&2 || true
+
+        # if still exist, move it somewhere that doesn't annoy us
+        mv "$HOME/Music" "$(xdg-user-dir MUSIC )/" 2>/dev/null || true
+    fi
+
+    #
+    # Images / Pictures
+    #
+    # FIXME: lots of confs of applications could reference to old dirs, make sure that we "upgrade" them too
+
+    if [[ -e "$HOME/Images" ]] ; then
+        mv "$HOME/"Images/* "$( xdg-user-dir PICTURES )/" 2>/dev/null || true
+
+        rmdir "$HOME/Images" 2>/dev/null 1>&2 || true
+
+        # if still exist, move it somewhere that doesn't annoy us
+        mv "$HOME/Images" "$(xdg-user-dir PICTURES )/" 2>/dev/null || true
+    fi
+
+    #
+    # Videos
+    #
+
+    if [[ -e "$HOME/Videos" ]] ; then
+        mv "$HOME/"Videos/* "$( xdg-user-dir VIDEOS )/" 2>/dev/null || true
+
+        rmdir "$HOME/Videos" 2>/dev/null 1>&2 || true
+
+        # if still exist, move it somewhere that doesn't annoy us
+        mv "$HOME/Videos" "$(xdg-user-dir VIDEOS )/" 2>/dev/null || true
+    fi
+
 
 
     # update again and save results
@@ -48,9 +127,17 @@ main(){
     # clean some files created by E17 which are useless:
     rm -f "$HOME/home.desktop" "$HOME/root.desktop" "$HOME/tmp.desktop"
 
+    #
+    # Publishare
+    #
+
     # Make the publicshare folder to be directly shared
     # net usershare add NAME DIR COMMENT ACL GUEST
     net usershare add "${USER}_$( basename "$(xdg-user-dir PUBLICSHARE )" )" "$(xdg-user-dir PUBLICSHARE )" "$USER Public directory in $HOSTNAME computer" Everyone:r guest_ok=yes   #2>/dev/null 1>&2 || true
+
+
+    # put back values
+    shopt -u dotglob
 }
 
 #

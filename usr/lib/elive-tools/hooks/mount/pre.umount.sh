dbb2c5013d10d93bcbc94bd3540c07f5232f3cf9
Thanatermesis <thanatermesis@gmail.com>
2014-07-05 16:05:19 +0200

    always kill tumblerd

--- usr/lib/elive-tools/hooks/mount/pre.umount.sh
+++ usr/lib/elive-tools/hooks/mount/pre.umount.sh
@@ -12,12 +12,11 @@ main(){
 
     # }}}
 
-
     # kill sucky process that doesn't allow us to umount devices
-    if top -b -n 1 | head -n 14 | tail -n 7  | grep -q tumblerd 2>/dev/null 1>/dev/null ; then
+    #if top -b -n 1 | head -n 14 | tail -n 7  | grep -q tumblerd 2>/dev/null 1>/dev/null ; then
         killall tumblerd 2>/dev/null 1>/dev/null
         killall -9 tumblerd 2>/dev/null 1>/dev/null
-    fi
+    #fi
 
 }
 

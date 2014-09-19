94687dfd5f1eb97f8304b671951db987e26dd152
Thanatermesis <thanatermesis@gmail.com>
2014-09-19 22:25:43 +0200

    run the informer of remote connections in background instead to wait for it

--- etc/profile.d/inform-remote-connection.sh
+++ etc/profile.d/inform-remote-connection.sh
@@ -1,1 +1,4 @@
-../zsh/zprofile.d/inform-remote-connection.sh
+#!/bin/bash
+
+# This tool informs to the user if somebody connected via ssh remotely from another country, comment/remove it if you found it annoying
+{ /usr/lib/elive-tools/hooks/shell/inform-remote-connection.sh & disown ; } 2>/dev/null

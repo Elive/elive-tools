84c1f4e37b298d9cc92e1871409d8d20038e3df5
Thanatermesis <thanatermesis@gmail.com>
2014-09-20 18:36:36 +0200

    force using of zsh or bash

--- etc/profile.d/inform-remote-connection.sh
+++ etc/profile.d/inform-remote-connection.sh
@@ -2,5 +2,5 @@
 
 # This tool informs to the user if somebody connected via ssh remotely from another country, comment/remove it if you found it annoying
 if [ -n "$BASH_VERSION" ] ; then
-    { /usr/lib/elive-tools/hooks/shell/inform-remote-connection.sh & disown ; } 2>/dev/null
+    { bash /usr/lib/elive-tools/hooks/shell/inform-remote-connection.sh & disown ; } 2>/dev/null
 fi
--- etc/zsh/zprofile.d/inform-remote-connection.sh
+++ etc/zsh/zprofile.d/inform-remote-connection.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 if [ -n "$ZSH_VERSION" ] ; then
-    /usr/lib/elive-tools/hooks/shell/inform-remote-connection.sh  &|
+    zsh /usr/lib/elive-tools/hooks/shell/inform-remote-connection.sh  &|
 fi
 

ababa107871446374addc88fc6542ed90f487c3b
Thanatermesis <thanatermesis@gmail.com>
2015-05-15 18:19:51 +0200

    fix

--- usr/lib/user-manager/hooks.d/e17-wizard/after/20-xdg-default-dirs.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/20-xdg-default-dirs.sh
@@ -415,18 +415,15 @@ main(){
     # create symlinks for thunar panel
     rm -f "$HOME/.gtk-bookmarks"
 
-    if ! grep -qs "file://$( xdg-user-dir DOWNLOAD | uri-gtk-encode )" "$HOME/.gtk-bookmarks" ; then
-        echo -e "file://$( xdg-user-dir DOWNLOAD | uri-gtk-encode )" >> "$HOME/.gtk-bookmarks"
-    fi
     if ! grep -qs "file://$( xdg-user-dir DOCUMENTS | uri-gtk-encode )" "$HOME/.gtk-bookmarks" ; then
         echo -e "file://$( xdg-user-dir DOCUMENTS | uri-gtk-encode )" >> "$HOME/.gtk-bookmarks"
     fi
     if ! grep -qs "file://$( xdg-user-dir VIDEOS | uri-gtk-encode )" "$HOME/.gtk-bookmarks" ; then
         echo -e "file://$( xdg-user-dir VIDEOS | uri-gtk-encode )" >> "$HOME/.gtk-bookmarks"
     fi
-    #if ! grep -qs "file://$( xdg-user-dir MUSIC | uri-gtk-encode )" "$HOME/.gtk-bookmarks" ; then
-        #echo -e "file://$( xdg-user-dir MUSIC | uri-gtk-encode )" >> "$HOME/.gtk-bookmarks"
-    #fi
+    if ! grep -qs "file://$( xdg-user-dir DOWNLOAD | uri-gtk-encode )" "$HOME/.gtk-bookmarks" ; then
+        echo -e "file://$( xdg-user-dir DOWNLOAD | uri-gtk-encode )" >> "$HOME/.gtk-bookmarks"
+    fi
     if ! grep -qs "file:///tmp" "$HOME/.gtk-bookmarks" ; then
         echo -e "file:///tmp Temporal" >> "$HOME/.gtk-bookmarks"
     fi

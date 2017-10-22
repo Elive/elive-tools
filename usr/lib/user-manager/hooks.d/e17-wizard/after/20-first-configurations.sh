6bad9274d97d10ac372f4a37260f3f629eed6cd1
Thanatermesis <thanatermesis@gmail.com>
2017-10-22 19:12:13 +0200

    small fixes

--- usr/lib/user-manager/hooks.d/e17-wizard/after/20-first-configurations.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/20-first-configurations.sh
@@ -16,9 +16,14 @@ main(){
         if [[ -e "$DHOME/.shared-home" ]] ; then
             el_explain 0 "home is shared"
             $guitool --warning --text="$( eval_gettext "Your home is shared with another system so we will not configure your audio card in order to have it working on your other system too, you should have it working in your Elive by default, but if is not the case please run the audio-configurator application and it will create a file in your home directory called '.asoundrc' (which starts with a dot) to make your audio working, if then your audio doesn't works in your other systems then you should need to delete it." )"
+
         else
             el_explain 0 "Configuring audio cards..."
             audio-configurator --quiet --auto --smart
+
+            # we need to create the PCM channel! it doesn't exist until we do it
+            timeout 5 aplay /dev/null 2>/dev/null
+            LC_ALL=C sleep 0.1
         fi
     fi
 

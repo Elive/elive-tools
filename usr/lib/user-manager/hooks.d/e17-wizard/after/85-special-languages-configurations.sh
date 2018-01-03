0a9bbbc2a07df553e56bf764d0e015bc44079c11
Thanatermesis <thanatermesis@gmail.com>
2018-01-03 22:26:24 +0100

    small fix

--- usr/lib/user-manager/hooks.d/e17-wizard/after/85-special-languages-configurations.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/85-special-languages-configurations.sh
@@ -44,7 +44,7 @@ main(){
         message_asking="$( printf "$( eval_gettext "Do you want to add support for %s keyboard input in your Elive system?" )" "$language" )"
 
         local message_instructions
-        message_instructions="$( printf "$( eval_gettext "To be able to type in %s, you need to change your keyboard input to '%s'. You can found this option typing '%s' in the launcher. Or in %s. Inside these settings you must select the '%s' button and then '%s', finally select the '%s' and start the daemon, then select the %s language in the second tab. Now you just need to open a graphical application and press '%s' to switch to your %s keyboard." )" "${language}" "Ibus" "Input Method Settings" "Menu -> Settings -> Language -> Input Methods" "System" "ibus" "Setup Option" "${language}" "Ctrl + Space" "${language}"  )"
+        message_instructions="$( printf "$( eval_gettext "To be able to type in %s, you need to change your keyboard input to '%s'. You can found this option typing '%s' in the launcher. Or in %s. Inside these settings you must select the '%s' button and then '%s', finally select the '%s' and start the daemon, then select the %s language in the second tab and the Add button. Now you just need to open a graphical application and press '%s' to switch to your %s keyboard." )" "${language}" "Ibus" "Input Method Settings" "Menu -> Settings -> Language -> Input Methods" "System" "ibus" "Setup Option" "${language}" "Ctrl + Space" "${language}"  )"
 
         # install
         if zenity --question --text="$message_asking" ; then

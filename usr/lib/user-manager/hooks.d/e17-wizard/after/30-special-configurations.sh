ad4d28f998dc62e67ed621d0842b525e6231f5b0
Thanatermesis <thanatermesis@gmail.com>
2018-02-20 19:17:08 +0100

    better support of disabling battery

--- usr/lib/user-manager/hooks.d/e17-wizard/after/30-special-configurations.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/30-special-configurations.sh
@@ -29,7 +29,10 @@ main(){
     fi
 
     # disable battery emodule if the battery is broken
-    if LC_ALL=C acpi 2>&1 | grep -qsE "\s+0%$" || LC_ALL=C acpi 2>&1 | grep -qsE "No support for.*power_supply" ; then
+    if LC_ALL=C acpi 2>&1 | grep -qsE "\s+(0|1|2)%$" \
+        || LC_ALL=C acpi 2>&1 | grep -qsE "No support for.*power_supply" \
+        || ! LC_ALL=C acpi 2>&1 | grep -qsE "\s+[[:digit:]]*%$" ; then
+
         enlightenment_remote -module-disable battery
         enlightenment_remote -module-unload battery
     fi

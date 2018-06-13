4a5619e638a8f406908ccd441cb6feaa8d261d0b
Thanatermesis <thanatermesis@gmail.com>
2018-06-13 03:22:43 +0200

    wording

--- usr/lib/user-manager/hooks.d/e17-wizard/after/85-special-languages-configurations.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/85-special-languages-configurations.sh
@@ -9,7 +9,7 @@ export TEXTDOMAIN
 
 suggest_emodule_flag_keyboard(){
     local message_suggestion_flag
-    message_suggestion_flag="$( printf "$( eval_gettext "Tip: Your country uses different keyboard layouts. If you want to switch between different language keyboards fastly, right click in the shelf of the bottom right corner to add as content the keyboard gadget." )" "" )"
+    message_suggestion_flag="$( printf "$( eval_gettext "Tip: Your country uses different keyboard layouts. If you want to switch between different language keyboards fastly, right click in the shelf of the bottom right corner to add as content the keyboard gadget. You can also add a hotkey to switch them fastly." )" "" )"
 
     zenity --info --text="$message_suggestion_flag"
 }

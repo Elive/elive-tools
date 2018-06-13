c91efc50e20e95a72fc081f2720ab997bef8683f
Thanatermesis <thanatermesis@gmail.com>
2018-06-13 03:21:56 +0200

    tip about keyboard layout switch in corner

--- usr/lib/user-manager/hooks.d/e17-wizard/after/85-special-languages-configurations.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/85-special-languages-configurations.sh
@@ -7,6 +7,13 @@ TEXTDOMAIN="elive-tools"
 export TEXTDOMAIN
 
 
+suggest_emodule_flag_keyboard(){
+    local message_suggestion_flag
+    message_suggestion_flag="$( printf "$( eval_gettext "Tip: Your country uses different keyboard layouts. If you want to switch between different language keyboards fastly, right click in the shelf of the bottom right corner to add as content the keyboard gadget." )" "" )"
+
+    zenity --info --text="$message_suggestion_flag"
+}
+
 
 main(){
     # pre {{{
@@ -20,19 +27,32 @@ main(){
         ko_KR*)
             language="Korean"
             package="ibus-hangul"
+            suggest_emodule_flag_keyboard
             ;;
         ja_JP*)
             language="Japanese"
             package="ibus-anthy,ibus-mozc"
+            suggest_emodule_flag_keyboard
             ;;
         zh_CN*|zh_TW*)
             language="Chinese"
             local message_instructions_extra
             message_instructions_extra="$( printf "$( eval_gettext "Use the Package Manager first to install one of the suggested options supporting Chinese input in different ways:" )" " ibus-cangjie, ibus-chewing, ibus-pinyin, ibus-sunpinyin" )"
+            suggest_emodule_flag_keyboard
             ;;
         vi_VN*)
             language="Vietnamese"
             package="ibus-unikey"
+            suggest_emodule_flag_keyboard
+            ;;
+        *_CA*|*_CH*|*_CM*|*_MA*|*_CD*|*_GH*|*_GN*|*_RS*|*_SY*|*_TR*|*_ZA*|*_KE*|*_AF*|*_BR*|*_HR*|*_CZ*|*_IR*|*_FI*|*_FR*|*_GE*|*_DE*|*_HU*|*_KZ*|*_NO*|*_PL*|*_ES*|*_SE*|*_UK*|*_PK*|*_NG*|*_ML*)
+            # countries with multiple layouts, info from /usr/share/doc/keyboard-configuration/xorg.lst
+            # canada, swiss, cameroon, morocco, congo, ghana, guinea, serbia, syria, turkish, south africa, kenya, afghanistan, brazil, croatia, czech, iran, finland, france, georgia, germany, hungary, kazakshtan, norwegian, polish, spain, sweden, ukrania, pakistan, nigeria, mali
+            suggest_emodule_flag_keyboard
+            ;;
+        *_RU*)
+            # russia has many different layouts!
+            suggest_emodule_flag_keyboard
             ;;
     esac
 

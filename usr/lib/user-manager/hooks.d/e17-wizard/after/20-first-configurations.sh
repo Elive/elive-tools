9e0c43c3fceeb4067bcc250d53caec311d3ae33e
Thanatermesis <thanatermesis@gmail.com>
2018-06-28 19:17:58 +0200

    do not reconfigure audio cards in every desktop restart

--- usr/lib/user-manager/hooks.d/e17-wizard/after/20-first-configurations.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/20-first-configurations.sh
@@ -11,23 +11,22 @@ main(){
     # }}}
 
     # Audio configurations {{{
-    if [[ -s "$HOME/.asoundrc" ]] ; then
-        mv -f "$HOME/.asoundrc" "$HOME/.asoundrc.bak"
-    else
+    if ! [[ -s "$HOME/.asoundrc" ]] ; then
         # special cases, having a .asoundr doesn't works in other non-elive systems
         if [[ -e "$DHOME/.shared-home" ]] ; then
             el_explain 0 "home is shared"
             $guitool --warning --text="$( eval_gettext "Your home is shared with another system so we will not configure your audio card in order to have it working on your other system too, you should have it working in your Elive by default, but if is not the case please run the audio-configurator application and it will create a file in your home directory called '.asoundrc' (which starts with a dot) to make your audio working, if then your audio doesn't works in your other systems then you should need to delete it." ) - Or delete the file $HOME/.shared-home"
 
+        else
+
+            el_explain 0 "Configuring audio cards..."
+            audio-configurator --quiet --auto --smart
+
+            # we need to create the PCM channel! it doesn't exist until we do it
+            timeout 5 aplay /dev/null 2>/dev/null
+            LC_ALL=C sleep 0.1
         fi
     fi
-
-    el_explain 0 "Configuring audio cards..."
-    audio-configurator --quiet --auto --smart
-
-    # we need to create the PCM channel! it doesn't exist until we do it
-    timeout 5 aplay /dev/null 2>/dev/null
-    LC_ALL=C sleep 0.1
 
     el_explain 0 "Setting default volumes..."
     rm -f "$HOME/.config/setvolume" 2>/dev/null 1>&2

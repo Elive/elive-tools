e3d1bb837e0a1be737b3e1be18e0b13421f5afec
Thanatermesis <thanatermesis@gmail.com>
2018-07-12 13:45:20 +0200

    fix for battery detection

--- usr/lib/user-manager/hooks.d/e17-wizard/after/10-emodules.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/10-emodules.sh
@@ -5,12 +5,19 @@ main(){
     # }}}
 
     # disable battery emodule if the battery is broken
-    if LC_ALL=C acpi 2>&1 | grep -qsE "\s+(0|1|2)%$" \
+    # checks:
+    # only 0/1/2% of battery remaining (broken values)
+    # battery not working/detected
+    # or we have no numeric values with % at all
+    if LC_ALL=C acpi 2>&1 | grep -qsE "\s+(0|1|2)%" \
         || LC_ALL=C acpi 2>&1 | grep -qsE "No support for.*power_supply" \
-        || ! LC_ALL=C acpi 2>&1 | grep -qsE "\s+[[:digit:]]*%$" ; then
+        || ! LC_ALL=C acpi 2>&1 | grep -qsE "\s+[[:digit:]]*%" ; then
 
         enlightenment_remote -module-disable battery
         enlightenment_remote -module-unload battery
+    else
+        enlightenment_remote -module-enable battery
+        enlightenment_remote -module-load battery
     fi
 
 

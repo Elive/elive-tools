48a0176747066c45133f7a42077858de20c9abe2
Thanatermesis <thanatermesis@gmail.com>
2019-02-11 20:06:35 -0500

    small improvements

--- etc/zsh/zprofile.d/colors-console.sh
+++ etc/zsh/zprofile.d/colors-console.sh
@@ -50,7 +50,7 @@ fi
 # dynamically use the same colorschemes that we have for our X terminals
 if [ "$TERM" = "linux" ]; then
     _SEDCMD='s/^[^\!].*\*color\([0-9]\{1,\}\).*#\([0-9a-fA-F]\{6\}\).*/\1 \2/p'
-    for i in $(sed -n "$_SEDCMD" "$HOME/.Xdefaults" | awk '$1 < 16 {printf "\\e]P%X%s", $1, $2}'); do
+    for i in $(sed -n "$_SEDCMD" "$HOME/.Xdefaults" 2>/dev/null | awk '$1 < 16 {printf "\\e]P%X%s", $1, $2}'); do
         echo -en "$i"
     done
     #clear

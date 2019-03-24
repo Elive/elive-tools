dee777238f71e10664c44d4cb142a45dcba1f906
Thanatermesis <thanatermesis@gmail.com>
2019-03-24 20:38:11 +0100

    optimizations to use SH instead of BASH

--- usr/lib/user-manager/hooks.d/e17-wizard/after/30-special-configurations.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/30-special-configurations.sh
@@ -1,38 +1,40 @@
-#!/bin/bash
+#!/bin/sh
 main(){
     # pre {{{
 
     # }}}
 
     # disable XV rendering if we cannot use it
-    if xvinfo | grep -qs "screen #" && xvinfo | grep -qs "no adaptors present" ; then
-        touch "$HOME/.mplayer/gui.conf" "$HOME/.mplayer/config"
+    if [ "$(which xvinfo)" ] ; then
+        if xvinfo | grep -qs "screen #" && xvinfo | grep -qs "no adaptors present" ; then
+            touch "$HOME/.mplayer/gui.conf" "$HOME/.mplayer/config"
 
-        if grep -qs "^vo_driver =" "$HOME/.mplayer/gui.conf" ; then
-            sed -i "s/^vo_driver.*$/vo_driver = \"x11\"/g" "$HOME/.mplayer/gui.conf"
-        else
-            echo "vo_driver = \"x11\"" >> "$HOME/.mplayer/gui.conf"
-        fi
+            if grep -qs "^vo_driver =" "$HOME/.mplayer/gui.conf" ; then
+                sed -i "s/^vo_driver.*$/vo_driver = \"x11\"/g" "$HOME/.mplayer/gui.conf"
+            else
+                echo "vo_driver = \"x11\"" >> "$HOME/.mplayer/gui.conf"
+            fi
 
-        if grep -qsE "^(vo=|#vo=)" "$HOME/.mplayer/config" ; then
-            sed -i "s|^#vo=.*$|vo=x11|g" "$HOME/.mplayer/config"
-            sed -i "s|^vo=.*$|vo=x11|g" "$HOME/.mplayer/config"
-        else
-            echo "vo=x11" >> "$HOME/.mplayer/config"
-        fi
-        if grep -qsE "^(zoom=|#zoom=)" "$HOME/.mplayer/config" ; then
-            sed -i "s|^#zoom=.*$|zoom=yes|g" "$HOME/.mplayer/config"
-            sed -i "s|^zoom=.*$|zoom=yes|g" "$HOME/.mplayer/config"
-        else
-            echo -e "zoom=yes" >> "$HOME/.mplayer/config"
+            if grep -qsE "^(vo=|#vo=)" "$HOME/.mplayer/config" ; then
+                sed -i "s|^#vo=.*$|vo=x11|g" "$HOME/.mplayer/config"
+                sed -i "s|^vo=.*$|vo=x11|g" "$HOME/.mplayer/config"
+            else
+                echo "vo=x11" >> "$HOME/.mplayer/config"
+            fi
+            if grep -qsE "^(zoom=|#zoom=)" "$HOME/.mplayer/config" ; then
+                sed -i "s|^#zoom=.*$|zoom=yes|g" "$HOME/.mplayer/config"
+                sed -i "s|^zoom=.*$|zoom=yes|g" "$HOME/.mplayer/config"
+            else
+                echo "zoom=yes" >> "$HOME/.mplayer/config"
+            fi
         fi
     fi
 
 
     # if we are debugging give it a little pause to see what is going on
-    if grep -qs "debug" /proc/cmdline ; then
-        echo -e "debug: sleep 2" 1>&2
-    fi
+    #if grep -qs "debug" /proc/cmdline ; then
+        #echo -e "debug: sleep 2" 1>&2
+    #fi
 }
 
 #

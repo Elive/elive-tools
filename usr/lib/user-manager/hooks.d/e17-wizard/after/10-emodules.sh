800612cb4e47558b326a16875a347b20ef7b048f
Thanatermesis <thanatermesis@gmail.com>
2019-03-24 20:01:24 +0100

    optimizations to SH instead of BASH

--- usr/lib/user-manager/hooks.d/e17-wizard/after/10-emodules.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/10-emodules.sh
@@ -1,42 +1,44 @@
-#!/bin/bash
+#!/bin/sh
 main(){
-    # pre {{{
-
-    # }}}
-
-    # disable battery emodule if the battery is broken
-    # checks:
-    # only 0/1/2% of battery remaining (broken values)
-    # battery not working/detected
-    # or we have no numeric values with % at all
-    if LC_ALL=C acpi 2>&1 | grep -qsE "\s+(0|1|2)%" \
-        || LC_ALL=C acpi 2>&1 | grep -qsE "No support for.*power_supply" \
-        || ! LC_ALL=C acpi 2>&1 | grep -qsE "\s+[[:digit:]]*%" ; then
-
-        enlightenment_remote -module-disable battery
-        enlightenment_remote -module-unload battery
+    if [ -n "$EROOT" ] ; then
+        # e16
+        true
     else
-        enlightenment_remote -module-load battery
-        enlightenment_remote -module-enable battery
-    fi
+        # e17
+        if [ -n "$E_START" ] && [ "$( which enlightenment_remote )" ] ; then
 
-    # always load screen configurations
-    if [[ -e "/usr/share/xdgeldsk/applications/arandr-load-conf.desktop" ]] ; then
-        mkdir -p "$HOME/.e/e17/applications/restart"
-        if ! grep -qs "arandr-load-conf.desktop" "$HOME/.e/e17/applications/restart/.order" ; then
-            echo "arandr-load-conf.desktop" >> "$HOME/.e/e17/applications/restart/.order"
+            # disable battery emodule if the battery is broken
+            # tested in: wheezy, buster
+            # checks in order:
+            # only 0/1/2% of battery remaining (broken values)
+            # battery not working/detected
+            # or we have no numeric values with % at all
+            acpi_result="$( LC_ALL=C acpi 2>&1 )"
+            if echo "$acpi_result" | grep -qsE "\s+(0|1|2)%" \
+                || echo "$acpi_result" | grep -qsE "No support for.*power_supply" \
+                || ! echo "$acpi_result" | grep -qsE "\s+[[:digit:]]*%" ; then
+
+                enlightenment_remote -module-disable battery
+                enlightenment_remote -module-unload battery
+            else
+                # enable battery by default
+                enlightenment_remote -module-load battery
+                enlightenment_remote -module-enable battery
+            fi
+
+            # always load screen configurations
+            if [ -e "/usr/share/xdgeldsk/applications/arandr-load-conf.desktop" ] ; then
+                mkdir -p "$HOME/.e/e17/applications/restart"
+                if ! grep -qs "arandr-load-conf.desktop" "$HOME/.e/e17/applications/restart/.order" ; then
+                    echo "arandr-load-conf.desktop" >> "$HOME/.e/e17/applications/restart/.order"
+                fi
+            fi
+
+
+            # save
+            sync ; sleep 1
+            enlightenment_remote -save
         fi
-    fi
-
-
-    # save
-    sync ; sleep 1
-    enlightenment_remote -save
-
-
-    # if we are debugging give it a little pause to see what is going on
-    if grep -qs "debug" /proc/cmdline ; then
-        echo -e "debug: sleep 2" 1>&2
     fi
 }
 

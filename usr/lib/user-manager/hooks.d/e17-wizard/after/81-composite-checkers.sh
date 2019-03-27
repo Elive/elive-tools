8fc621e9cdd49ef2db06f33f6efeba6a9bcc3489
Thanatermesis <thanatermesis@gmail.com>
2019-03-27 13:23:20 +0100

    support composite for e16

--- usr/lib/user-manager/hooks.d/e17-wizard/after/81-composite-checkers.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/81-composite-checkers.sh
@@ -18,43 +18,14 @@ main(){
     # pre {{{
     local file dir temp
 
-    # e16
-    if [[ -n "$EROOT" ]] ; then
-        # nothing to do
-        return
-    fi
-
-    # }}}
     # already shown, ignore
     if el_flag check "composite-details" ; then
         return
     fi
 
-    # default E profile of user
-    dir="${HOME}/.e/e17/config/$(enlightenment_remote -default-profile-get)"
+    # }}}
 
-    if [[ ! -d "$dir" ]] ; then
-        el_warning "Unable to get default E profile of user, switching to default one"
-        dir="${HOME}/.e/e17/config/standard"
-    fi
-    if [[ ! -d "$dir" ]] ; then
-        el_error "Where is the default E profile of user? exiting..."
-        exit
-    fi
-
-    # get file
-    file="${dir}/module.comp.cfg"
-    if [[ ! -s "$file" ]] ; then
-        # if not exist, most probably is because we have it disabled (no composite at all selected)
-        el_debug "E composite conf not found, ignoring..."
-        exit
-    fi
-
-    # extract file
-    temp="$(mktemp --suffix .src )"
-    eet -d "$file" config "$temp"
-
-    # know virtualized state
+    # virtualized? {{{
     if ! [[ -s "/tmp/.lshal" ]] || ! [[ "$( wc -l "/tmp/.lshal" | cut -f 1 -d ' ' )" -gt 100 ]] ; then
         /usr/sbin/hald
         sync
@@ -68,61 +39,112 @@ main(){
         is_virtualized=1
     fi
 
+    # }}}
 
-    # get data
+    # e16 {{{
+    if [[ -n "$EROOT" ]] ; then
 
-    value="$( grep 'value "engine"' "$temp" | sed -e 's|^.*: ||g' -e 's|;.*$||g' | tail -1 )"
-    case "$value" in
-        1)
-            local message_gl
-            message_gl="$( printf "$( eval_gettext "No desktop acceleration selected. This option is more stable, but it may result in a less responsive desktop, especially during video playback. If you did not try the accelerated mode yet, it is suggested to select it. You will be able to see, then , if it is compatible with your graphic card." )" )"
+        if zenity --question --text="$( eval_gettext "Do you want to enable composite layer? It will give you transparencies and a nicer looking desktop." )" ; then
+            eesh compmgr start
+            is_composite_e16_started=1
+        else
+            eesh compmgr stop
+        fi
 
-            if ! ((is_virtualized)) ; then
-                zenity --info --text="$message_gl" || true
+        if eesh -w "compmgr ?" | grep -qs "on=1" ; then
+            zenity --info --text="$( eval_gettext "Composite in E16 should give you a better experience,  but if you see any issue or a reason to not enable it by default in Elive in the future, please share your comments in our forums. You can disable it in any moment from the configurations." )"
+        else
+            # report if failed
+            if ((is_composite_e16_started)) ; then
+                el_warning "User wanted to enable composite but it failed? $(lspci | grep VGA)"
             fi
-            ;;
-        2)
-            if ((is_virtualized)) ; then
-                local message_vbox
-                message_vbox="$( printf "$( eval_gettext "The hardware acceleration mode may not work correctly in a virtual machine" )" "" )"
-                zenity --warning --text="$message_vbox"
+        fi
+    fi
 
-            else
+    # }}}
+    # e17 {{{
+    if [[ -n "$E_START" ]] ; then
+        # default E profile of user
+        dir="${HOME}/.e/e17/config/$(enlightenment_remote -default-profile-get)"
+
+        if [[ ! -d "$dir" ]] ; then
+            el_warning "Unable to get default E profile of user, switching to default one"
+            dir="${HOME}/.e/e17/config/standard"
+        fi
+        if [[ ! -d "$dir" ]] ; then
+            el_error "Where is the default E profile of user? exiting..."
+            exit
+        fi
+
+        # get file
+        file="${dir}/module.comp.cfg"
+        if [[ ! -s "$file" ]] ; then
+            # if not exist, most probably is because we have it disabled (no composite at all selected)
+            el_debug "E composite conf not found, ignoring..."
+            exit
+        fi
+
+        # extract file
+        temp="$(mktemp --suffix .src )"
+        eet -d "$file" config "$temp"
+
+
+        # show some info to the users
+        value="$( grep 'value "engine"' "$temp" | sed -e 's|^.*: ||g' -e 's|;.*$||g' | tail -1 )"
+        case "$value" in
+            1)
                 local message_gl
-                message_gl="$( printf "$( eval_gettext "Hardware acceleration makes your desktop more responsive and a smoother feeling, improves also the video playback speed and quality. But if the drivers for your graphic card are not correctly supported it can lead to a broken desktop or window visuals, to fix this you will need to switch to software mode in the composite options or disable the acceleration in a new desktop configuration." )" )"
+                message_gl="$( printf "$( eval_gettext "No desktop acceleration selected. This option is more stable, but it may result in a less responsive desktop, especially during video playback. If you did not try the accelerated mode yet, it is suggested to select it. You will be able to see, then , if it is compatible with your graphic card." )" )"
 
-                zenity --info --text="$message_gl" || true
-
-                # vsync ?
-                if grep 'value "vsync"' "$temp" | sed -e 's|^.*: ||g' -e 's|;.*$||g' | tail -1 | grep -qs "1" ; then
-                    true
-                else
-                    local message_vsync_disabled
-                    message_vsync_disabled="$( printf "$( eval_gettext "You did not select the %s option for composite (vertical synchronization). This option allows you to play videos perfectly smoothly and without horizontal lines. Go to the options panel, and in the options panel, go to the composite section." )" "vsync" )"
-                    zenity --info --text="$message_vsync_disabled" || true
+                if ! ((is_virtualized)) ; then
+                    zenity --info --text="$message_gl" || true
                 fi
-            fi
+                ;;
+            2)
+                if ((is_virtualized)) ; then
+                    local message_vbox
+                    message_vbox="$( printf "$( eval_gettext "The hardware acceleration mode may not work correctly in a virtual machine" )" "" )"
+                    zenity --warning --text="$message_vbox"
 
-            # intel card for wheezy?
-            # update: not needed anymore
-            #if lspci | grep VGA | grep -qs "Intel" ; then
+                else
+                    local message_gl
+                    message_gl="$( printf "$( eval_gettext "Hardware acceleration makes your desktop more responsive and a smoother feeling, improves also the video playback speed and quality. But if the drivers for your graphic card are not correctly supported it can lead to a broken desktop or window visuals, to fix this you will need to switch to software mode in the composite options or disable the acceleration in a new desktop configuration." )" )"
+
+                    zenity --info --text="$message_gl" || true
+
+                    # vsync ?
+                    if grep 'value "vsync"' "$temp" | sed -e 's|^.*: ||g' -e 's|;.*$||g' | tail -1 | grep -qs "1" ; then
+                        true
+                    else
+                        local message_vsync_disabled
+                        message_vsync_disabled="$( printf "$( eval_gettext "You did not select the %s option for composite (vertical synchronization). This option allows you to play videos perfectly smoothly and without horizontal lines. Go to the options panel, and in the options panel, go to the composite section." )" "vsync" )"
+                        zenity --info --text="$message_vsync_disabled" || true
+                    fi
+                fi
+
+                # intel card for wheezy?
+                # update: not needed anymore
+                #if lspci | grep VGA | grep -qs "Intel" ; then
                 #if grep debian-version /etc/elive-version | grep -i wheezy ; then
-                    #local message_intel_buggy
-                    #message_intel_buggy="$( printf "$( eval_g
+                #local message_intel_buggy
+                #message_intel_buggy="$( printf "$( eval_g
                 #ettext "Note Intel cards: There's a known problem with the blanking (screensaver) powersavign feature on this version of the Intel drivers, which turns your desktop unrensponsive, if you really want automatic screen blanking you should use instead the software-mode of composite, but if you don't need it, just don't turn it on and everything else is fine." )" )"
 
-                    #zenity --info --text="$message_intel_buggy" || true
+                #zenity --info --text="$message_intel_buggy" || true
                 #fi
-            #fi
+                #fi
 
-            ;;
-    esac
+                ;;
+        esac
+
+        # cleanups
+        rm -f "$temp"
+
+    fi
+    # end e17 }}}
 
 
     el_flag add "composite-details"
-
-    # cleanups
-    rm -f "$temp"
 
 
     # if we are debugging give it a little pause to see what is going on

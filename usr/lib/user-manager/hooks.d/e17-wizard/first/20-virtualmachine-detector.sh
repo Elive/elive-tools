bfb56e02acab335fcdb39549f67b05cbdce62dc4
Thanatermesis <thanatermesis@gmail.com>
2019-05-21 16:17:49 +0200

    small improvements

--- usr/lib/user-manager/hooks.d/e17-wizard/first/20-virtualmachine-detector.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/first/20-virtualmachine-detector.sh
@@ -4,14 +4,14 @@ main(){
     # pre {{{
     #local file dir temp
 
-    if [[ -e "/etc/elive/machine-profile" ]] ; then
+    if [ -e "/etc/elive/machine-profile" ] ; then
         source /etc/elive/machine-profile
     fi
     # }}}
 
     # This is needed for the (next) composite wizard page 150 modifications:
-    if [[ "$MACHINE_VIRTUAL" = "yes" ]] ; then
-        touch "/tmp/.virtualmachine-detected" 2>/dev/null
+    if [ "$MACHINE_VIRTUAL" = "yes" ] && [ -z "$EROOT" ] ; then
+        touch "/tmp/.virtualmachine-detected"
         chmod a+rw "/tmp/.virtualmachine-detected" 2>/dev/null
     fi
 }

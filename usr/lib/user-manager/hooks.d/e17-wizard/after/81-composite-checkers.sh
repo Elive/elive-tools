b8a073092af416c5894480036056ce0c7c6bd808
Thanatermesis <thanatermesis@gmail.com>
2020-11-30 20:24:50 -0500

    auto: untarzanice | PLEASE review this commit @Thanatermesis

--- usr/lib/user-manager/hooks.d/e17-wizard/after/81-composite-checkers.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/after/81-composite-checkers.sh
@@ -99,7 +99,7 @@ main(){
         case "$value" in
             1)
                 local message_gl
-                message_gl="$( printf "$( eval_gettext "Hardware-accelerated desktop not used. This option is more stable, but it may result in a slower desktop, especially during video playback. If you did not try the accelerated mode yet, it is suggested to try it. You will be able to see, then, if it is compatible with your graphic card." )" )"
+                message_gl="$( printf "$( eval_gettext "Hardware-accelerated desktop not used. This option is more stable, but it may result in a slower desktop, especially during video playback. If you did not try the accelerated mode yet, it is suggested to try it. You will be able to see, then, if it is compatible with your graphics card." )" )"
 
                 if ! [[ "$MACHINE_VIRTUAL" = "yes" ]] ; then
                     zenity --info --text="$message_gl" || true
@@ -108,12 +108,12 @@ main(){
             2)
                 if [[ "$MACHINE_VIRTUAL" = "yes" ]] ; then
                     local message_vbox
-                    message_vbox="$( printf "$( eval_gettext "The hardware acceleration mode may not work correctly in a virtual machine" )" "" )"
+                    message_vbox="$( printf "$( eval_gettext "The hardware acceleration mode may not work correctly in a virtual machine." )" "" )"
                     zenity --warning --text="$message_vbox"
 
                 else
                     local message_gl
-                    message_gl="$( printf "$( eval_gettext "Hardware acceleration makes your desktop faster and having a smoother feeling, it also improves video playback speed and its quality. But if the drivers of your graphic card are not enough stable, it can lead to a broken desktop or with visual artifacts, to fix this you will need to switch to software mode in the compositor options or disable the acceleration in a new desktop configuration." )" )"
+                    message_gl="$( printf "$( eval_gettext "Hardware acceleration makes your desktop faster and having a smoother feeling, it also improves video playback speed and its quality. But if the drivers of your graphic card are not stable enough, it can lead to a broken desktop and/or visual artifacts, to fix this you will need to switch to software mode in the compositor options or disable the acceleration in a new desktop configuration." )" )"
 
                     zenity --info --text="$message_gl" || true
 

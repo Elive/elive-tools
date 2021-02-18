75dbc23dc4260d6dcf9cde5eb2a460fda3e761f7
Thanatermesis <thanatermesis@gmail.com>
2021-02-18 11:18:12 -0500

    cleanups

--- usr/lib/user-manager/hooks.d/e17-wizard/first/30-font-scale-autodetection.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/first/30-font-scale-autodetection.sh
@@ -85,33 +85,6 @@ main(){
         sed -i -e "/last-window-height/s/value=\".*\"/value=\"${thunar_window_height}\"/g" "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml"
     fi
 
-        # TODO: define a scaling factor value to configure gnome-3 and elementary (which will include terminology and E too aparently)
-        # TODO: VERIFY which file affects this:
-        #gsettings set org.gnome.desktop.interface scaling-factor 2
-        # TODO configure elementary manually?
-        # TODO: terminals like urxvt (font size?)
-
-        # TODO: create a file called ~/.config/xfce4/xfconf/xfce-perchannel-xml/thunar.xml  in elive-skel files, FROM a new thunar default window opened, and change THEIR relative values to the OBTAINED ones like on this example:
-#--- a/xfce4/xfconf/xfce-perchannel-xml/thunar.xml	2021-01-21 15:29:04.000000000 -0500
-#+++ b/xfce4/xfconf/xfce-perchannel-xml/thunar.xml	2021-01-21 15:31:15.245482016 -0500
-#@@ -3,9 +3,9 @@
- #<channel name="thunar" version="1.0">
-   #<property name="last-view" type="string" value="ThunarIconView"/>
-   #<property name="last-location-bar" type="string" value="ThunarLocationEntry"/>
-#-  <property name="last-icon-view-zoom-level" type="string" value="THUNAR_ZOOM_LEVEL_100_PERCENT"/>
-#-  <property name="last-window-width" type="int" value="1347"/>
-#-  <property name="last-window-height" type="int" value="859"/>
-#+  <property name="last-icon-view-zoom-level" type="string" value="THUNAR_ZOOM_LEVEL_400_PERCENT"/>
-#+  <property name="last-window-width" type="int" value="1764"/>
-#+  <property name="last-window-height" type="int" value="983"/>
-   #<property name="last-window-maximized" type="bool" value="false"/>
-#-  <property name="last-separator-position" type="int" value="315"/>
-#+  <property name="last-separator-position" type="int" value="279"/>
-
-
-        # TODO: set statuc average values for improved size:
-            # Note: While you can set any dpi you like and applications using Qt and GTK will scale accordingly, it's recommended to set it to 96, 120 (25% higher), 144 (50% higher), 168 (75% higher), 192 (100% higher) etc., to reduce scaling artifacts to GUI that use bitmaps. Reducing it below 96 dpi may not reduce size of graphical elements of GUI as typically the lowest dpi the icons are made for is 96.
-
 
     # load settings
     xrdb -merge "$HOME/.Xdefaults"

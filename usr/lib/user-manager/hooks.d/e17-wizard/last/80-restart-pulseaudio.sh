33f1aa0b4cc24cad0c008e9b84a925e0c466ac5c
Thanatermesis <thanatermesis@gmail.com>
2024-02-29 11:52:02 -0500

    restart pulseaudio on new desktop

---
+++ usr/lib/user-manager/hooks.d/e17-wizard/last/80-restart-pulseaudio.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+main(){
+    if [ -x "$(which pulseaudio)" ] && [ -e /var/lib/dpkg/info/pulseaudio.list ] ; then
+        pulseaudio -k 2>/dev/null || true
+        pulseaudio -D
+        LC_ALL=C sleep 2
+
+        if ! pulseaudio --check 1>/dev/null 2>&1 ; then
+            pulseaudio -k 2>/dev/null || true
+            pulseaudio -D
+        fi
+        LC_ALL=C sleep 2
+
+        if ! pactl info 1>/dev/null 2>&1 ; then
+            pulseaudio -k 2>/dev/null || true
+            pulseaudio -D
+        fi
+    fi
+}
+
+#
+#  MAIN
+#
+main "$@"
+
+# vim: set foldmethod=marker :

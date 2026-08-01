49a70dff5bc282b8968c368c8ae0a3b99310b908
Thanatermesis <thanatermesis@gmail.com>
2026-08-01 10:41:56 +0700

    typo

--- usr/lib/user-manager/hooks.d/e17-wizard/first/15-precache.sh
+++ usr/lib/user-manager/hooks.d/e17-wizard/first/15-precache.sh
@@ -3,9 +3,9 @@
 precache(){
     # precache desktop files in BG while we wait for language to select
     if [ -n "$EROOT" ] ; then
-        bash -c "precache --nice /etc/xdg/autostart/*desktop /usr/share/applications/*desktop /usr/share/xdgeldsk/applications/*desktopp $(which cairo-dock) $(which conky) $(which zenity) $(which yad) /usr/lib/notification-daemon/notification-daemon   1>/dev/null 2>&1  & disown"
+        bash -c "precache --nice /etc/xdg/autostart/*desktop /usr/share/applications/*desktop /usr/share/xdgeldsk/applications/*desktop $(which cairo-dock) $(which conky) $(which zenity) $(which yad) /usr/lib/notification-daemon/notification-daemon   1>/dev/null 2>&1  & disown"
     elses
-        bash -c "precache --nice /etc/xdg/autostart/*desktop /usr/share/applications/*desktop /usr/share/xdgeldsk/applications/*desktopp $(which zenity) $(which yad)  1>/dev/null 2>&1  & disown"
+        bash -c "precache --nice /etc/xdg/autostart/*desktop /usr/share/applications/*desktop /usr/share/xdgeldsk/applications/*desktop $(which zenity) $(which yad)  1>/dev/null 2>&1  & disown"
     fi
 }
 
